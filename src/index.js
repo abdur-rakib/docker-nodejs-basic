fd4189c4953488dd6ca5b9323aa228001e57a761
Abdur Rakib <abdurrakib961@gmail.com>
2023-09-17 16:58:12 +0600

    setup docker

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ require("dotenv").config();
 const app = express();
 
 app.get("/", (req, res) => {
+  console.log("Basic route hitted!!!");
   return res.json({ message: "Docker basic route!!!" });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`App is running on port: ${process.env.PORT || 3000}`);
